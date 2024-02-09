71b6d5036f9c8fd4478732fb380278ca2ae20438
Matt McMeans <mrmcmans@memphis.edu>
2024-02-09 12:44:02 -0600

    EntityFramework changes

---
+++ Data/BloggieDbContext.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Bloggie.Web.Data
+{
+    public class BloggieDbContext : DbContext
+    {
+        public BloggieDbContext(DbContextOptions<BloggieDbContext> options) : base(options)
+        {
+        }
+    }
+}
